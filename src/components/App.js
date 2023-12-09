253bbdb6079f280e2b0c4db1ca1cd00e7b73cf80
Bogdan <bogdantisenko06@gmail.com>
2023-12-10 00:59:04 +0200

    finishFix

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,15 @@ import { GlobalStyle } from './GlobalStyled';
 import { ContactForm } from './Form/Form';
 import { ContactList } from './ContactList';
 import { SearchBar } from './SearchBar';
-import { useDispatch, useSelector } from 'react-redux';
-import { add } from '../redux/contactsSlice';
+import { useSelector } from 'react-redux';
 
 export const App = () => {
   const contacts = useSelector(state => state.contacts.contacts);
-  const dispatch = useDispatch();
-
-  const addContact = newContact => {
-    const { name, number } = newContact;
-
-    if (!name || !number) {
-      alert('Please fill in both name and number fields.');
-      return;
-    }
-    const contactExists = contacts.some(
-      contact =>
-        contact.name.toLowerCase() === name.toLowerCase() ||
-        contact.number === number
-    );
-
-    if (contactExists) {
-      alert('Contact with this name or number already exists.');
-      return;
-    }
-    dispatch(add(newContact));
-  };
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm addContact={addContact} />
+      <ContactForm />
       <h2>Contacts</h2>
       <SearchBar />
       {contacts.length > 0 && <ContactList />}
